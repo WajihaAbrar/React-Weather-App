fc8be7be5f871364e3803002a5e65ad75c4bd7c1
WajihaAbrar <wajihaabrar@gmail.com>
2024-02-09 14:05:55 +0000

    Added the GitHub repo URL.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        The project was coded by Wajiha Abrar and is open-sourced on{" "}
+        <a
+          href="https://github.com/WajihaAbrar/React-Weather-App"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://weather-forecast-app-react.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
